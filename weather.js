b02769aca63f1588fb4339da4c18077ae76ac2bd
Alexander <alexandrmozolevskiy@gmail.com>
2022-07-31 15:56:28 +0300

    chalk and dedent added

--- weather.js
+++ weather.js
@@ -1,11 +1,12 @@
 #!usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { printHelp } from './services/log.service.js'
 
 const initCLI = () => {
     const args = getArgs(process.argv)
-    console.log(args)
     if (args.h) {
         // Help
+        printHelp()
     }
     if (args.s) {
         // Save
