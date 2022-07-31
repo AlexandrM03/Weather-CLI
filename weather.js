590f1e6a027717a823a43cb508f4a690f24d8c33
Alexander <alexandrmozolevskiy@gmail.com>
2022-07-31 16:28:54 +0300

    Token save and edit

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js'
+import { printHelp, printSuccess, printError } from './services/log.service.js'
+import { saveKeyValue } from './services/storage.service.js'
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Токен сохранён')
+    } catch (e) {
+        printError(e.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
@@ -13,6 +23,7 @@ const initCLI = () => {
     }
     if (args.t) {
         // Token
+        return saveToken(args.t)
     }
 }
 
