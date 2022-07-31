7a4ee4aa26d6843e48b65057531135ab316bce78
Alexander <alexandrmozolevskiy@gmail.com>
2022-07-31 15:40:14 +0300

    Args added

---
+++ weather.js
@@ -0,0 +1,18 @@
+#!usr/bin/env node
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    console.log(args)
+    if (args.h) {
+        // Help
+    }
+    if (args.s) {
+        // Sity
+    }
+    if (args.t) {
+        // Token
+    }
+}
+
+initCLI()
